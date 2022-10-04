06fed075e0effd2c3b8c3d75791ff6252eace540
Joven Rosales <104308582+JayVen02@users.noreply.github.com>
2022-10-04 09:15:57 +0800

    Add files via upload

---
+++ DateTime Excercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DateTime_Excercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myDate = DateTime.Now;
+            string formattedDate = "";
+            // First Format
+            formattedDate = string.Format("{0:dd-MM-yyyy HH:mm:ss}",myDate );
+            Console.WriteLine(formattedDate);
+            //Second Format
+            formattedDate = string.Format("{0:dddd MMMM yyyy}", myDate);
+            Console.WriteLine(formattedDate);
+            //Third Format
+            formattedDate = string.Format("{0:dddd \nMMMM \nyyyy }",myDate);
+            Console.WriteLine(formattedDate);
+            Console.ReadLine();
+        }
+    }
+}
