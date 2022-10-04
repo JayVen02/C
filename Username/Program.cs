9bbd9d927c16a79ba8b5c2a55dfd442cb61577e4
Joven Rosales <104308582+JayVen02@users.noreply.github.com>
2022-10-04 09:17:09 +0800

    Add files via upload

---
+++ Username/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Username
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Username: ");
+
+            string Username = Console.ReadLine();
+
+            Console.WriteLine("Username is: " + Username);        }
+    }
+}
